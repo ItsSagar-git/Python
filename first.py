e8c2cfa585f52c90a52a98b9dbd63b2bf64e08b9
Sagar Addhikari <sagar.adhikary675@gmail.com>
2024-05-21 22:08:09 +0545

    File Added

--- first.py
+++ first.py
@@ -6,3 +6,5 @@ print("tata")
 print("hi bro \n whats up") #Escape Sequence
 '''lets learn about comments''' #Multi line Comments
 print("hi \"iam a good boy\'") #Escape sequence
+print("hey" ,6,sep="#",end="999") #sep=seperate with the symbol. end=last character before another string
+print("Sagar") ##Default seperator
