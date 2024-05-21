adb4e70ecdb0d81e50391c1ca66006bd00dc3b45
Sagar Addhikari <sagar.adhikary675@gmail.com>
2024-05-21 22:30:56 +0545

    Last Commit of the day

--- Calculator.py
+++ Calculator.py
@@ -3,5 +3,6 @@ b=15
 add=a+b
 sub=a-b
 mul=a*b
-div=a/b
-print(add,"\n",sub,"\n",mul,"\n",div)
+div=a//b
+exp=a**b
+print("Addition=",add,"\n","Subtraction=",sub,"\n","Multiply=",mul,"\n","Division=",div,"\n","Exponential=",exp)
